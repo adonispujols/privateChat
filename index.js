66a8982de6756411db728f9786f436b15e4d020a
adonispujols <adonispujols@users.noreply.github.com>
2018-01-06 21:37:41 -0500

    minor fixes and added comments

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+// setting up app
 var app = require('express')();
-var http = require('http');
+var http = require('http').Server(app);   // Ensure we're using HTTPS
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/index.html");
+// serving initial files from server to client
+app.get('/', function(req, res){          // GET okay here (just serving html)?
+    // serve index
+    res.sendFile(__dirname + '/index.html');
+});
+
+// once user joins, do function
+io.on('connection', function(socket){
+    console.log('a user connected');  // logs to node.js server/terminal
+    // once user disconnect, do functino
+    socket.on('disconnect', function(){
+        console.log('user disconnected')
+    })
+});
+
+// http server listens to certain port
+http.listen(3000, function(){
+    console.log('listening on *:3000');  // log upon starting
 });
